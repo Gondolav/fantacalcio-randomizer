d3d44677be6f83ff31de6dd203370725bc42ee78
Andrea Veneziano <andrea.veneziano@epfl.ch>
2019-09-06 18:33:23 +0200

    Update layout

--- fantacalcio-randomizer/gui.py
+++ fantacalcio-randomizer/gui.py
@@ -28,14 +28,18 @@ def add_teams_names_entries(n_teams_entry):
 
 
 # create a GUI variable called app
-app = gui("Fantacalcio Randomizer", "600x300")
+app = gui("Fantacalcio Randomizer", "400x200")
 app.setFont(18)
+app.setLocation("CENTER")
+app.setGuiPadding(4, 4)
 
 # add & configure widgets - widgets get a name, to help referencing them later
 app.addFileEntry("Quotazioni")
+app.setEntryDefault("Quotazioni", "Aggiungere il file excel delle quotazioni")
 
 app.addLabelEntry("Quante squadre? ")
 app.setEntrySubmitFunction("Quante squadre? ", add_teams_names_entries)
+app.setEntryDefault("Quante squadre? ", "ex: 8")
 
 # start the GUI
 app.go()
