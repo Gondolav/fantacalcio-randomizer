896ed2502268301c54c0cf87d01145c7083223a5
Andrea Veneziano <andrea.veneziano@epfl.ch>
2019-09-06 11:56:45 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='fantacalcio-randomizer',
+      version='1.0',
+      description='A randomizer to generate Fantacalcio teams',
+      author='Andrea Veneziano',
+      author_email='andrea.veneziano@icloud.com',
+      url='https://github.com/Gondolav/fantacalcio-randomizer',
+      py_modules=['fantacalcio-randomizer'],
+      requires=['xlutils', 'appjar']
+     )
